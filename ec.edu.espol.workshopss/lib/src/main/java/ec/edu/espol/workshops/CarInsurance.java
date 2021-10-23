ab2fd0adfc9e498428e40628ddce42bdaa8170db
RicardoVilcacundo <rvilcacu@espol.edu.ec>
2021-10-22 23:08:09 -0500

    Bug de los espacios arreglados.

--- ec.edu.espol.workshopss/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshopss/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,14 +1,15 @@
+
 package ec.edu.espol.workshops;
 
 public class CarInsurance {
 	public static void main(String[] args) {
-		Cliente cl1= new Cliente(24,"M","no casado",true);
+		Cliente cl1 = new Cliente(24, "M", "no casado", true);
 		Compañia com = new Compañia();
 		System.out.println(cl1);
 		System.out.println(com.calcularPrima(cl1));
-		
-	}	
-	
+
+	}
+
 }
 
 class Cliente {
@@ -16,76 +17,64 @@ class Cliente {
 	private String sexo;
 	private String estado;
 	private boolean permiso;
-	
+
 	public Cliente(int edad, String sexo, String estado, boolean permiso) {
 		this.edad = edad;
 		this.sexo = sexo;
-		this.estado= estado;
+		this.estado = estado;
 		this.permiso = permiso;
 	}
-	
-	public int getEdad() {
+	public int getEdad(){
 		return edad;
 	}
-	
-	public void setEdad(int edad) {
+	public void setEdad(int edad){
 		this.edad = edad;
 	}
-	
-	public String getSexo() {
+	public String getSexo(){
 		return sexo;
 	}
-	
-	public void setSexo(String sexo) {
+	public void setSexo(String sexo){
 		this.sexo = sexo;
 	}
-	
-	public String getEstado() {
+	public String getEstado(){
 		return estado;
 	}
-	
-	public void setEstado(String estado) {
+	public void setEstado(String estado){
 		this.estado = estado;
 	}
-	
-	public boolean getPermiso() {
+	public boolean getPermiso(){
 		return permiso;
 	}
-	
-	public void setPermiso(boolean permiso) {
+	public void setPermiso(boolean permiso){
 		this.permiso = permiso;
 	}
-	public String toString() {
-		return "Edad:" + edad + " " + "sexo:" + sexo + " " + "Estado civil:" + estado + " " + "Permiso de conducir:" + permiso;
+	public String toString(){
+		return "Edad:" + edad + " " + "sexo:" + sexo + " " + "Estado civil:" + estado + " " + "Permiso de conducir:"
+				+ permiso;
 	}
-
 }
 
-
 class Compañia {
-	
-	int primaBase=500;
-
-	public int calcularPrima(Cliente cliente) {
+	int primaBase = 500;
+  public int calcularPrima(Cliente cliente) {
 		Cliente cl = cliente;
-		if(cl.getEdad()>80) {
+		if (cl.getEdad() > 80) {
 			System.out.println("Debido a su edad no cumple los requisitos");
 			return -1;
 		}
-		if(!cl.getPermiso()) {
+		if (!cl.getPermiso()) {
 			System.out.println("No tiene permiso de conducir");
 			return -1;
 		}
-		if(cl.getSexo()=="M" & cl.getEstado()== "no casado" & cl.getEdad()<25) {
+		if (cl.getSexo() == "M" & cl.getEstado() == "no casado" & cl.getEdad() < 25) {
 			primaBase = primaBase + 1500;
 		}
-		if(cl.getSexo()=="F" | cl.getEstado()== "casado") {
+		if (cl.getSexo() == "F" | cl.getEstado() == "casado") {
 			primaBase = primaBase - 200;
 		}
-		if(cl.getEdad()>=45 & cl.getEdad()<65) {
+		if (cl.getEdad() >= 45 & cl.getEdad() < 65) {
 			primaBase = primaBase - 100;
 		}
 		return primaBase;
 	}
 }
-
