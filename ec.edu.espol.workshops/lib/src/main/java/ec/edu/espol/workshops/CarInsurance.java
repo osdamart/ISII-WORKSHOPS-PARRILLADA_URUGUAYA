c1a20a76c28710948464492be401cdd7126ec22e
Oscar Martínez <osdamart@espol.edu.ec>
2021-10-29 19:12:07 -0500

    fixed errors

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,92 +1,91 @@
 package ec.edu.espol.workshops;
 
 public class CarInsurance {
+	public static void main(String[] args) {
+		Cliente cl1 = new Cliente(-24, "M", "no casado", true);
+		Compania com = new Compania();
+		System.out.println(cl1);
+		System.out.println(com.calcularPrima(cl1));
 
-public static void main(String[] args) {
-
-Cliente cl1 = new Cliente(24, "M", "no casado", true);
-
-Compañia com = new Compañia();
-
-System.out.println(cl1);
-System.out.println(com.calcularPrima(cl1));
-
-
+	}
 
 }
-
-
-
-}
-
-
 
 class Cliente {
-private int edad;
-private String sexo;
-private String estado;
-private boolean permiso;
+	private int edad;
+	private String sexo;
+	private String estado;
+	private boolean permiso;
 
-public Cliente(int edad, String sexo, String estado, boolean permiso) {
-this.edad = edad;
-this.sexo = sexo;
-this.estado = estado;
-this.permiso = permiso;
+	public Cliente(int edad, String sexo, String estado, boolean permiso) {
+		this.edad = edad;
+		this.sexo = sexo;
+		this.estado = estado;
+		this.permiso = permiso;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public boolean getPermiso() {
+		return permiso;
+	}
+
+	public void setPermiso(boolean permiso) {
+		this.permiso = permiso;
+	}
+
+	public String toString() {
+		return "Edad:" + edad + " " + "sexo:" + sexo + " " + "Estado civil:" + estado + " " + "Permiso de conducir:"
+				+ permiso;
+	}
 
 }
-public int getEdad() {
-return edad;
-}
-public void setEdad(int edad) {
-this.edad = edad;
-}
-public String getSexo() {
-return sexo;
-}
-public void setSexo(String sexo) {
-this.sexo = sexo;
-}
-public String getEstado() {
-return estado;
-}
-public void setEstado(String estado) {
-this.estado = estado;
-}
-public boolean getPermiso() {
-return permiso;
-}
-public void setPermiso(boolean permiso) {
-this.permiso = permiso;
-}
-public String toString() {
-return "Edad:" + edad + " " + "sexo:" + sexo + " " + "Estado civil:" + estado + " " + "Permiso de conducir:"
-+ permiso;
-}
-}
 
+class Compania {
 
+	int primaBase = 500;
 
-class Compañia {
-int primaBase = 500;
-public int calcularPrima(Cliente cliente) {
-Cliente cl = cliente;
-if (cl.getEdad() > 80) {
-System.out.println("Debido a su edad no cumple los requisitos");
-return -1;
-}
-if (!cl.getPermiso()) {
-System.out.println("No tiene permiso de conducir");
-return -1;
-}
-if (cl.getSexo() == "M" & cl.getEstado() == "no casado" & cl.getEdad() < 25) {
-primaBase = primaBase + 1500;
-}
-if (cl.getSexo() == "F" | cl.getEstado() == "casado") {
-primaBase = primaBase - 200;
-}
-if (cl.getEdad() >= 45 & cl.getEdad() < 65) {
-primaBase = primaBase - 100;
-}
-return primaBase;
-}
+	public int calcularPrima(Cliente cliente) {
+		Cliente cl = cliente;
+		if (cl.getEdad() > 80 || cl.getEdad() < 21) {
+			System.out.println("Debido a su edad no cumple los requisitos");
+			return -1;
+		}
+		if (!cl.getPermiso()) {
+			System.out.println("No tiene permiso de conducir");
+			return -1;
+		}
+		if (cl.getSexo().toUpperCase().equals("M") & cl.getEstado().equals("no casado") & cl.getEdad() < 25) {
+			primaBase = primaBase + 1500;
+		}
+		if (cl.getSexo().toUpperCase().equals("F") | cl.getEstado().equals("casado")) {
+			primaBase = primaBase - 200;
+		}
+		if (cl.getEdad() >= 45 & cl.getEdad() < 65) {
+			primaBase = primaBase - 100;
+		}
+		return primaBase;
+	}
 }
