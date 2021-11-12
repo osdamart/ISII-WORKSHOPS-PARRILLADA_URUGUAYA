77edea7ef67864ddfe1279f16eb8d440c4ef800f
Oscar Martínez <osdamart@espol.edu.ec>
2021-11-12 16:40:22 -0500

    correccion systemTesting

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,21 +1,24 @@
-package ec.edu.espol.workshops;
-
 public class CarInsurance {
+
 	public static void main(String[] args) {
-		Cliente cl1 = new Cliente(-24, "M", "no casado", true);
+// TODO Auto-generated method stub
+		Cliente cl1 = new Cliente(24, "M", "no casado", true);
+		Cliente cl2 = new Cliente(25, "F", "", true);
 		Compania com = new Compania();
 		System.out.println(cl1);
+		System.out.println(cl2);
 		System.out.println(com.calcularPrima(cl1));
+		System.out.println(com.calcularPrima(cl2));
 
 	}
 
 }
 
 class Cliente {
-	private int edad;
-	private String sexo;
-	private String estado;
-	private boolean permiso;
+	private int edad; // Variable para la edad de la persona.
+	private String sexo; // Variable para ver que sexo es la persona.
+	private String estado; // Variable para guardar si esta casado.
+	private boolean permiso; // Variable para guardar el permiso.
 
 	public Cliente(int edad, String sexo, String estado, boolean permiso) {
 		this.edad = edad;
@@ -64,12 +67,14 @@ class Cliente {
 }
 
 class Compania {
-
 	int primaBase = 500;
 
 	public int calcularPrima(Cliente cliente) {
 		Cliente cl = cliente;
-		if (cl.getEdad() > 80 || cl.getEdad() < 21) {
+		if (!(cl.getEstado() == "casado" | cl.getEstado() == "no casado")) {
+			return -1;
+		}
+		if (cl.getEdad() > 80) {
 			System.out.println("Debido a su edad no cumple los requisitos");
 			return -1;
 		}
@@ -77,10 +82,10 @@ class Compania {
 			System.out.println("No tiene permiso de conducir");
 			return -1;
 		}
-		if (cl.getSexo().toUpperCase().equals("M") & cl.getEstado().equals("no casado") & cl.getEdad() < 25) {
+		if (cl.getSexo().equals("M") & cl.getEstado().equals("no casado") & cl.getEdad() < 25) { // Correguido
 			primaBase = primaBase + 1500;
 		}
-		if (cl.getSexo().toUpperCase().equals("F") | cl.getEstado().equals("casado")) {
+		if (cl.getSexo().equals("F") | cl.getEstado().equals("casado")) { // Correguido
 			primaBase = primaBase - 200;
 		}
 		if (cl.getEdad() >= 45 & cl.getEdad() < 65) {
